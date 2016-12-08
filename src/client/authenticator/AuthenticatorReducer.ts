f7574a52d5fcba95a577df2ac09ebea532ba8f10
Jon Kenoyer <kenoyer130@gmail.com>
2016-12-07 21:39:02 -0500

    fix reducer to properly copy state
    * add handlers for violation and state form changed actions

--- src/client/authenticator/AuthenticatorReducer.ts
+++ src/client/authenticator/AuthenticatorReducer.ts
@@ -3,39 +3,44 @@ import { AuthenticatorStore } from './AuthenticatorStore'
 import { DefaultState } from './AuthenticatorStore'
 import { IAuthenticatorSettingsResponseAction } from './actions/AuthenticatorSettingsResponseAction'
 import { AUTHENTICATION_SETTINGS_RESPONSE } from './actions/AuthenticatorSettingsResponseAction'
-import { HAS_ACCOUNT_ACTION_RESPONSE } from './actions/HasAccountResponseAction'
-import { IHasAccountActionResponse } from './actions/HasAccountResponseAction'
+import { HAS_ACCOUNT_ACTION_RESPONSE , IHasAccountActionResponse} from './actions/HasAccountResponseAction'
+import { STATE_FORM_VALUE_CHANGED, IStateFormValuedChangeAction } from '../actions/StateFormValuedChangeAction'
+import { VIOLATION_ACTION, IViolationAction} from '../actions/ViolationAction'
+import { CREATE_ACCOUNT_ACTION, ICreateAccountAction} from '../newAccount/actions/CreateAccountAction'
 
 function AuthenticatorReducer(state : AuthenticatorStore = DefaultState, action : Redux.Action):AuthenticatorStore {
     switch(action.type) {
         case AUTHENTICATION_SETTINGS_RESPONSE:
             const settingsAction = action as IAuthenticatorSettingsResponseAction;
-           // state.update('authenticatorSettingsLoaded', authenticatorSettingsLoaded => settingsAction.authenticatorSettings != null);
-           // state.update('authenticatorSettings', authenticatorSettings => settingsAction.authenticatorSettings);
-            let test: AuthenticatorStore = {
-                isLocal: state.isLocal,
-                hasAuthToken: false,
+            
+            return Object.assign({}, state, {
                 authenticatorSettingsLoaded: true,
-                authenticatorSettings: settingsAction.authenticatorSettings,
-                authToken: null,
-                hasAccount: false,
-                accountEmail: null
-            }; 
-            return test;
-
+                authenticatorSettings: settingsAction.authenticatorSettings });
+         
          case HAS_ACCOUNT_ACTION_RESPONSE:
             const authenticatedAction = action as IHasAccountActionResponse;
+            
+            return Object.assign({}, state, { 
+                hasAuthToken: authenticatedAction.token != null,
+                authToken: authenticatedAction.token, 
+                hasAccount: authenticatedAction.accountExists, 
+                accountEmail: authenticatedAction.email});
+         
+         case STATE_FORM_VALUE_CHANGED:
+            const stateFormValuedChangeAction = action as IStateFormValuedChangeAction;
+            
+            let newState = Object.assign({}, state);
+            newState[stateFormValuedChangeAction.key] = stateFormValuedChangeAction.value
+            
+            return newState;
+        
+        case VIOLATION_ACTION:
+            const violationAction = action as IViolationAction;
+            return Object.assign({}, state, {
+                valid: violationAction.valid,
+                violation: violationAction.violation
+            });
 
-            let d: AuthenticatorStore = {
-                isLocal: state.isLocal,
-                hasAuthToken: true,
-                authToken: authenticatedAction.token,
-                authenticatorSettingsLoaded: state.authenticatorSettingsLoaded,
-                hasAccount: authenticatedAction.accountExists,
-                accountEmail: authenticatedAction.email
-            } 
-
-            return d;
         default:
             return state;
     }
