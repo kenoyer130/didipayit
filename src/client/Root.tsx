335af3d328b5191ad0c589ff8bbe4f7c85a0b319
Jon Kenoyer <kenoyer130@gmail.com>
2016-12-07 21:35:34 -0500

    properly set up routing in Root
    * add prop authentication check and routing for authentication

--- src/client/Root.tsx
+++ src/client/Root.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import {Router, Route, hashHistory} from 'react-router'
+import {Router, Route, hashHistory, browserHistory} from 'react-router'
 import AuthenticatorContainer from "./authenticator/containers/AuthenticatorContainer"
+import AccountSignupContainer from "./newAccount/AccountSignupContainer"
 
 export interface IRootProps {
     hasAuthToken : boolean,
@@ -11,22 +12,39 @@ export interface IRootState {};
 
 class Root extends React.Component<IRootProps, IRootState> {
 
-    onEnter = (nextState, replace, callback) => {
-        if(!this.props.hasAuthToken) {
-            replace('/login');
-        } else if(!this.props.hasAccount) {
-            replace('/account/new')
-        }
+   routes: any;
 
-        callback();
+    constructor(props) {
+        super(props);
+
+        this.routes = <Route path="/" >
+                        <Route path="/login" component={AuthenticatorContainer} />
+                        <Route path="/account/new" component={AccountSignupContainer} />
+                     </Route>;
     }
 
+    componentDidMount() {
+        this.checkAuth(this.props);
+    }
+
+    componentWillReceiveProps(nextProps:IRootProps) {
+        this.checkAuth(nextProps);
+    }
+   
+    checkAuth = (props) => {
+         if(!props.hasAuthToken && hashHistory.getCurrentLocation().pathname !== 'login') {
+            hashHistory.push("login");            
+            return;
+        }
+ 
+        if(!props.hasAccount && hashHistory.getCurrentLocation().pathname !== 'account/new') {
+            hashHistory.push("account/new");            
+            return;
+        }
+    };
+
     render() {
-       return <Router history={hashHistory}>
-            <Route path="/" onEnter={this.onEnter} >
-                <Route path="/login" component={AuthenticatorContainer} />
-            </Route>
-        </Router>
+       return  <Router history={hashHistory}>{this.routes}</Router>;
     }
 }
 
